315656642e3821d1135d6c52bb80e8e27b0f7492
Yewon Eliana Kim <elianakim@kaist.ac.kr>
2021-12-14 01:36:03 +0900

    demo 4

--- demo.py
+++ demo.py
@@ -5,12 +5,82 @@ import time
 import os
 import pandas as pd
 
+def drop_the_beat(type, dynamic):
+    if type == 0: # 2 beats
+        if dynamic == 'p':
+            print("   ##       ##    ")
+            print("   # #      # #   ")
+            print("  ## #     ## #   ")
+            print("####     ####     ")
+            print("###      ###      ")
+        elif dynamic == 'f':
+            print("         ###                 ###        ")
+            print("         ###                 ###        ")
+            print("         ####                ####       ")
+            print("         ### ##              ### ##     ")
+            print("         ###  ##             ###  ##    ")
+            print("         ###   ##            ###   ##   ")
+            print("         ###  ##             ###  ##    ")
+            print("         ###   ##            ###   ##   ")
+            print("   #######             #######          ")
+            print(" ##########          ##########         ")
+            print("  #########           #########         ")
+            print("   ######              ######           ")
+    elif type == 1: # 3 beats
+        if dynamic == 'p':
+            print("   ##       ##       ##    ")
+            print("   # #      # #      # #   ")
+            print("  ## #     ## #     ## #   ")
+            print("####     ####     ####     ")
+            print("###      ###      ###      ")
+        elif dynamic == 'f':
+            print("         ###                 ###                 ###        ")
+            print("         ###                 ###                 ###        ")
+            print("         ####                ####                ####       ")
+            print("         ### ##              ### ##              ### ##     ")
+            print("         ###  ##             ###  ##             ###  ##    ")
+            print("         ###   ##            ###   ##            ###   ##   ")
+            print("         ###  ##             ###  ##             ###  ##    ")
+            print("         ###   ##            ###   ##            ###   ##   ")
+            print("   #######             #######             #######          ")
+            print(" ##########          ##########          ##########         ")
+            print("  #########           #########           #########         ")
+            print("   ######              ######              ######           ")
+    elif type == 2: # 4 beats
+        if dynamic == 'p':
+            print("   ##       ##       ##       ##    ")
+            print("   # #      # #      # #      # #   ")
+            print("  ## #     ## #     ## #     ## #   ")
+            print("####     ####     ####     ####     ")
+            print("###      ###      ###      ###      ")
+        elif dynamic == 'f':
+            print("         ###                 ###                 ###                 ###        ")
+            print("         ###                 ###                 ###                 ###        ")
+            print("         ####                ####                ####                ####       ")
+            print("         ### ##              ### ##              ### ##              ### ##     ")
+            print("         ###  ##             ###  ##             ###  ##             ###  ##    ")
+            print("         ###   ##            ###   ##            ###   ##            ###   ##   ")
+            print("         ###  ##             ###  ##             ###  ##             ###  ##    ")
+            print("         ###   ##            ###   ##            ###   ##            ###   ##   ")
+            print("   #######             #######             #######             #######          ")
+            print(" ##########          ##########          ##########          ##########         ")
+            print("  #########           #########           #########           #########         ")
+            print("   ######              ######              ######              ######           ")
+
+
 def main():
     #f = open("results/beats_211212_meta_re_lr0.1_feat_re.txt")
-    f = open("results/beats_211212_meta_re_lr0.1_feat_re_dynamic.csv")
-    df = pd.read_csv(f)
+    # demo 3
+    f_dynamics = open("results/demo_beats_change_3_dynamic.csv")
+    df = pd.read_csv(f_dynamics)
     beats = df['beats']
     dynamic = df['dynamic']
+    f_types = open("results/demo_beats_type_3.csv")
+    typedf = pd.read_csv(f_types)
+    type = typedf['type']
+
+
+
     '''
     for line in f:
         values = re.split(",|\n", line)
@@ -28,6 +98,7 @@ def main():
         _dynamic = dynamic[i]
         if _beat > 0:
             if i == 0:
+                # print("%s" % type[i/2])
                 print("##########")
                 print("##########")
                 print("##########")
@@ -36,59 +107,10 @@ def main():
                 print("##########")
                 time.sleep(0.33)
                 os.system('clear')
-            elif _dynamic == 'f':
-                print("         ###")
-                print("         ###")
-                print("         ####")
-                print("         ### ##")
-                print("         ###  ##")
-                print("         ###   ##")
-                print("         ###  ##")
-                print("         ###   ##")
-                print("   #######")
-                print(" ##########")
-                print("  #########")
-                print("  ######")
+            else:
+                drop_the_beat(type[i//2], _dynamic)
                 time.sleep(0.33)
                 os.system('clear')
-            elif _dynamic == 'p':
-                print("   ##")
-                print("   # #")
-                print("  ## #")
-                print("####")
-                print("###")
-                time.sleep(0.33)
-                os.system('clear')
-
-
-        # if beat == 1:
-        #     print("----------")
-        #     print("--#####---")
-        #     print("----###---")
-        #     print("----###---")
-        #     print("----###---")
-        #     print("--#######-")
-        #     time.sleep(0.33)
-        #     os.system('clear')
-        #     # play(beep)
-        # elif beat == 2:
-        #     print("-########-")
-        #     print("------###-")
-        #     print("-########-")
-        #     print("-###------")
-        #     print("-###------")
-        #     print("-########-")
-        #     time.sleep(0.33)
-        #     os.system('clear')
-        # elif beat == 3:
-        #     print("#########-")
-        #     print("------###-")
-        #     print("-########-")
-        #     print("------###-")
-        #     print("#########-")
-        #     print("----------")
-        #     time.sleep(0.33)
-        #     os.system('clear')
         else: # beat is 0
             # wait for 330ms
             time.sleep(0.33)
