f6461c915d0239a9e074dd8d7429af81055e517b
Yewon Eliana Kim <elianakim@kaist.ac.kr>
2021-12-20 18:53:32 +0900

    remove unnecessary overhead

--- script/train_models.sh
+++ script/train_models.sh
@@ -30,18 +30,10 @@ python main.py --type beat_change --dataset beat_original --model beat_change_mo
 # CNN light model - Beat Detector
 #
 
-# multi-class
-python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts2_lr0.001_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 2
-python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts3_lr0.001_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 3
-python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts4_lr0.001_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 4
 # binary
 python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts2_b_lr0.001_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 2 --binary
 python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts3_b_lr0.001_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 3 --binary
 python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts4_b_lr0.001_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 4 --binary
-# multi-class + downsample
-python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts2_lr0.001_ds_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 2 --downsample
-python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts3_lr0.001_ds_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 3 --downsample
-python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts4_lr0.001_ds_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 4 --downsample
 # binary + downsample
 python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts2_b_lr0.001_ds_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 2 --binary --downsample
 python main.py --type beat_change --dataset beat_original --model beat_change_model_light --method Src --log_suffix bd_reproduce_ts3_b_lr0.001_ds_light --feat_eng --lr 0.001 --gpu_idx 0 --beat_type 3 --binary --downsample
